f9034889b04cdac240594435866df3c90cd2d5cf
Vanhoang1999 <vanhoang.htra@gmail.com>
2019-05-29 22:01:16 +0700

    Toán tử số học

---
+++ src/lab2/toangtusohoc.java
@@ -0,0 +1,24 @@
+package lab2;
+
+class toangtusohoc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int  A= 20 ;
+		int B = 20 ;
+		int C, D, E, F ;
+		//phep cong
+		C = A + B;
+		System.out.println( "A + B =" +C);
+		//phep tru
+		D = A - B;
+		System.out.println( "A - B =" +D);
+		// phep nhan
+		E = A * B;
+		System.out.println( "A * B =" +E);
+		//phep chia
+		F = A / B;
+		System.out.println( "A / B =" +F);
+	}
+
+}
