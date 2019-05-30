cbec8e8ad061ad77091d252b6afbe41a7cf01287
Vanhoang1999 <vanhoang.htra@gmail.com>
2019-05-30 16:35:24 +0700

    Vòng lặp for

--- ifelse/src/ifelse/tinhdiemtrunbienh.java
+++ ifelse/src/ifelse/tinhdiemtrunbienh.java
@@ -9,20 +9,24 @@ class tinhdiemtrunbienh {
 		String address;
 		double diemhk1, diemhk2, diemtrungbinh;
 		String ketQuaXepLoai;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Nhập vào tên sinh viên: ");
-        name = scanner.nextLine();
-        System.out.println("Nhập năm sinh: ");
-        year = Integer.parseInt(scanner.nextLine());//kháº¯c phuc hiá»‡n tÆ°á»£ng trÃ´i lá»‡nh
-        System.out.println("Địa chỉ: ");
-        address = scanner.nextLine();
+		
+		for (int i=1; i<6; i++) {
+			System.out.println(i);
+		
+		Scanner scanner = new Scanner(System.in);
         
-         
-        System.out.println("Nhập điểm TB hk1:");
+        System.out.println("Nhập vào tên sinh viên: " );
+        name = scanner.nextLine();
+        System.out.println("Nhập năm sinh: " );
+        year = Integer.parseInt(scanner.nextLine());//Khắc phục lỗi trôi lệnh
+        System.out.println("Địa chỉ: " );
+        address = scanner.nextLine();
+        System.out.println("Nhập điểm TB hk1:" );
         diemhk1 = scanner.nextDouble();
         System.out.println("Nhập điểm TB hk2: ");
         diemhk2= scanner.nextDouble();
         diemtrungbinh = ((diemhk1 + diemhk2*2.0)/3);
+        
         if (diemtrungbinh >= 8.5) {
             ketQuaXepLoai = "Loại A";
         } else if ((diemtrungbinh >= 7) && (diemtrungbinh < 8.5)) {
@@ -31,16 +35,15 @@ class tinhdiemtrunbienh {
             ketQuaXepLoai = "Loại C";
         } else if ((diemtrungbinh > 4) && (diemtrungbinh< 5.5)) {
             ketQuaXepLoai = "Loại D";
-        } else {
+        } else  {
             ketQuaXepLoai = "Loại F";
         }
         
-         
         System.out.println("Họ tên sinh viên: " + name);
         System.out.println("Năm sinh :  " + year);
         System.out.println("Địa chỉ: " + address);
         System.out.println("Điểm Tb cả năm : " + diemtrungbinh);
         System.out.println("Kết quả xếp loại môn học của sinh viên là " + ketQuaXepLoai);
-        }
-	
+		}		
+	}
 	}
