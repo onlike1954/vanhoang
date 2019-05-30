2fa3512ad3fc30e293a36127d9267d3b8c6d9dbc
Vanhoang1999 <vanhoang.htra@gmail.com>
2019-05-30 20:31:00 +0700

    kiểm tra số chẵn lẻ

---
+++ ifelse/src/ifelse/kiemtrasochanle.java
@@ -0,0 +1,25 @@
+package ifelse;
+
+import java.util.Scanner;
+
+public class kiemtrasochanle
+{
+	public static void main(String[] args)
+	{
+		int number;
+		Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào số cần kiểm tra: ");
+        number = scanner.nextInt();
+         
+        // số chẵn là số chia hết cho 2
+        if (number % 2 == 0) 
+        {
+            System.out.println(number + " là số chẵn");
+        }
+        else {
+            // ngược lại là số lẻ
+            System.out.println(number + " là số lẻ");
+        	}
+        scanner.close();
+	}
+}
